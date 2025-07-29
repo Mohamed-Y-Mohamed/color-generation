1a1980f50022235e067a661906e197d3b69c60cb
Mohamed Yusuf Mohamed <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2025-07-29 02:41:41 +0100

    url update for allowed domain for api request

--- src/services/groqService.ts
+++ src/services/groqService.ts
@@ -9,7 +9,7 @@ function validateOrigin(): boolean {
   // Added directly here without needing environment variables
   const allowedOrigins = [
     "localhost:3000",
-    "elaborate-sunflower-c1e195.netlify.app", // Added the allowed origin directly
+    "https://smart-color-generator.netlify.app/", // Added the allowed origin directly
   ];
   const currentHost = window.location.host;
 
